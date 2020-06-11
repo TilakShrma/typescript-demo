ec256af6b5319099592c3febda11fc22753dc2d8
Tilak sharma <tilsharm@partnet.cisco.com>
2020-06-11 20:22:30 +0530

    ts advanced types

--- ts/types.ts
+++ ts/types.ts
@@ -3,3 +3,35 @@ const bool : boolean = true;
 
 // number
 const num : number = 50;
+
+// string
+const str: string = 'helloooo';
+
+// tuple -> can be used for maintaining ordered lists
+let basket: [string, number];
+basket = ['Cake', 1];
+
+// objects
+let car: object;
+car = {
+    engine: '1hp',
+    price: 2,
+};
+
+// enum
+enum Size {
+    Small = 1,
+    Medium = 2,
+    Large = 3,
+};
+
+
+// void
+let noReturn = (): void => {
+    console.log('only console log')
+}
+
+// never -> if a function have unreachable endpoint or does not return anything
+let err = (): never => {
+    throw Error('i am error');
+}
